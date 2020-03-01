31944ae74ce53282aefb8f446d7a0dc31acc7aa4
Kevin Li Zhong Hao <kevinli@Kevins-MacBook-Pro-2.local>
2020-02-29 20:42:30 -0500

    i cry

---
+++ src/comp346pa2w2020/Comp346pa2driver.java
@@ -0,0 +1,38 @@
+package comp346pa2w2020;
+
+import comp346pa1w2020.Server;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kerly Titus
+ */
+public class Comp346pa2driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    	Network objNetwork = new Network( );            /* Activate the network */
+        objNetwork.start();
+        Server objServer = new Server("thread1");                        /* Start the server */ 
+        objServer.start();
+        Server objServer2 = new Server("thread2");                        /* Start the server */ 
+        objServer2.start();
+        Client objClient1 = new Client("sending");          /* Start the sending client thread */
+        objClient1.start();
+        Client objClient2 = new Client("receiving");        /* Start the receiving client thread */
+        objClient2.start();
+        
+      /*..............................................................................................................................................................*/
+       
+    }
+    
+ }
